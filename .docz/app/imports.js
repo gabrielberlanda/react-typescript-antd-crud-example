a482d1de39c4f4719dcf6034a69e0b3a48868979
Gabriel Berlanda <gabriel.berlanda@INTELLTECH.LOCAL>
2019-06-18 17:56:36 -0300

    Revert docz

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  'src/components/StatusIcon/StatusIcon.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-components-status-icon-status-icon" */ 'src/components/StatusIcon/StatusIcon.mdx'
-    ),
-}
